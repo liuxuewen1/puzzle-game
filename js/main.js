5fcb637579c3a15d2581175f95dcf207c675207f
liuxuewen <liuxuewen@huli.com>
2017-11-11 01:47:43 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,48 @@
 $(function(){
+        
+    // 分享相关代码
+    var shareData = {
+        title: 'pierre cardin 70th 时光荣耀 拼图赢好礼！',
+        description: '三幅皮尔.卡丹女装拼图，拼出2017秋冬最闪耀，赢500元电子代金券！',
+        url: 'http://www.nihaoyo.com/',
+        imageUrl: 'http://www.nihaoyo.com/img/share.png'
+    };
+    var shareCommon = {
+        title: shareData.title,
+        link: shareData.url,
+        imgUrl: shareData.imageUrl
+    };
+    var wx_shareData_friend = shareCommon;
+    var wx_shareData_circle = {
+        title: shareData.title,
+        link: shareData.url,
+        imgUrl: shareData.imageUrl,
+        desc: shareData.description,
+        type: 'link'
+    };
+    
+    var wxShare = function(){
+        $.ajax({
+            url: 'https://liuxuewensyd.localtunnel.me/serve/jssign?url='+ encodeURIComponent(window.location.href.split('#')[0]),
+            type:'get',
+            dataType: 'json',
+            async:true,
+            success:function(data){
+                if(data.errorCode===0){
+                    wx.config(data.result);
+                    //微信分享注入
+                    wx.ready(function(){
+                        // 分享给好友
+                        wx.onMenuShareTimeline(wx_shareData_friend);
+                        // 朋友圈
+                        wx.onMenuShareAppMessage(wx_shareData_circle);
+                    });
+                }
+            }
+        })
+    };
+        
+
         var objData = {
             1: {
                 otime: 8,
@@ -187,7 +231,7 @@ $(function(){
                 "3": 500
             }
             $("#lotteryimg").attr('src', 'img/win'+lottery+'.jpeg');
-            $("#winmoney").text(objMoney[lottery]);
+            $("#winmoney").html('恭喜您获得<br>'+objMoney[lottery]+'电子代金券一张');
             $(".win").show();
             $(".popup-succ").hide();
             // $("#popup-mask").show();
